061e900f2a97e778eeec0fe7f05b4e82cc1e4120
emorilebo <emorylebo@gmail.com>
2021-10-20 22:26:33 +0100

    Fetching data

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Giphy = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+        params: {
+          api_key: "z1SvCQFyE00BezeOovuh2exg7afvchI7",
+        },
+      });
+      console.log(results);
+    };
+    fetchData();
+  });
   return <div>Giphy</div>;
 };
 
